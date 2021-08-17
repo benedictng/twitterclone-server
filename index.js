9a6ce8590c802b314d5295bc21267e3e77cfa00c
benedictng <69000787+benedictng@users.noreply.github.com>
2021-08-17 23:51:56 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require("cors")
+
+app.use(express.json());
+app.use(cors())
+
+const db = require('./models');
+
+//Routers
+const postRouter = require('./routes/posts')
+app.use("/posts", postRouter);
+const commentsRouter = require('./routes/comments')
+app.use("/comments", commentsRouter);
+const usersRouter = require("./routes/users");
+app.use("/auth", usersRouter);
+const likesRouter = require("./routes/likes");
+app.use("/likes", likesRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("Server running on port 3001")
+    });
+});
+
+
+
