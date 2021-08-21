442f99f6bff59cb98dfa2ae6c48042ecd98014d2
benedictng <69000787+benedictng@users.noreply.github.com>
2021-08-21 17:02:46 +0800

    reinstall node_modules

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const likesRouter = require("./routes/likes");
 app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log("Server running on port 3001")
     })
 })
